e8427bd128c9cf9e53a15bfe364dc3f37a18eebd
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2023-02-22 21:41:20 +0100

    verfijningen

--- chapter_5_GPIO/les3_stepper.py
+++ chapter_5_GPIO/les3_stepper.py
@@ -24,10 +24,11 @@ time.sleep(0.5)
 # Define some settings
 StepCounter = 0
 WaitTime = 0.0015
+fourStepSeq = [0,1,2,3]
  
 #Wave drive
 StepCount1 = 4
-Seq1 = [0,1,2,3]
+Seq1 = fourStepSeq 
 Seq1[0] = [1,0,0,0]
 Seq1[1] = [0,1,0,0]
 Seq1[2] = [0,0,1,0]
@@ -35,24 +36,11 @@ Seq1[3] = [0,0,0,1]
 
 #Full step
 StepCount2 = 4
-Seq2 = [0,1,2,3]
+Seq2 = fourStepSeq
 Seq2[0] = [1,1,0,0]
 Seq2[1] = [0,1,1,0]
 Seq2[2] = [0,0,1,1]
 Seq2[3] = [1,0,0,1]
-
-# Define advanced sequence
-# as shown in manufacturers datasheet
-StepCount3 = 8
-Seq3 = [0,1,2,3,4,5,6,7]
-Seq3[0] = [1,0,0,0]
-Seq3[1] = [1,1,0,0]
-Seq3[2] = [0,1,0,0]
-Seq3[3] = [0,1,1,0]
-Seq3[4] = [0,0,1,0]
-Seq3[5] = [0,0,1,1]
-Seq3[6] = [0,0,0,1]
-Seq3[7] = [1,0,0,1]
 
 # set
 Seq = Seq2 ## full step
@@ -62,10 +50,10 @@ StepCount = StepCount2
 try:
   while 1==1:
     for pin in range(0, 4):
-      xpin = StepPins[pin]
-      if Seq[StepCounter][pin]!=0:
-        print (" Step %i Enable %i" %(StepCounter,xpin))
-        wp.digitalWrite(xpin, True)
+      xpin = StepPins[pin] ## get GPIO number
+      if Seq[StepCounter][pin]!=0: ## check if pin in sequence is 1 or 0
+        print (" Step %i Enable %i" %(StepCounter,xpin)) 
+        wp.digitalWrite(xpin, True) ## set pin high
       else:
         wp.digitalWrite(xpin, False)
     StepCounter += 1
