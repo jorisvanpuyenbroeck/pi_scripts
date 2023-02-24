048571d5e824a44deef199d0ef7395a02bbddaaa
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2023-02-24 21:51:15 +0100

    lab 5.2

--- CrashCourse.py
+++
@@ -1,9 +0,0 @@
-
-def countto(j) :
-    i = 0
-    while i < j:
-        print(i, end=" ")
-        i+=1
-    return None
-
-countto(60)
---
+++ chapter_5_GPIO/lab5.2.2_pullup.py
@@ -0,0 +1,40 @@
+import time
+import wiringpi
+import sys
+
+## practice 1
+
+def blink(_pin):
+    wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
+    time.sleep(0.5)
+    wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
+    time.sleep(0.5)
+
+
+#SETUP
+print("Start")
+led_pin = 1
+switch_pin = 2
+
+wiringpi.wiringPiSetup() 
+wiringpi.pinMode(switch_pin, 0)         # Set pin to mode 0 ( INPUT )
+wiringpi.pinMode(led_pin, 1)            # Set pin to mode 1 ( OUTPUT )
+
+#MAIN
+try: 
+    while True:
+        if(wiringpi.digitalRead(switch_pin) == 0): #input is active low (pull up)
+            print("LED flashing")
+            time.sleep(0.3) #anti bouncing
+            blink(led_pin)
+        else:
+            print("LED not flashing")
+            time.sleep(0.3) #anti bouncing
+            wiringpi.digitalWrite(led_pin, 0)    # Write 0 ( LOW ) to LED
+
+
+except KeyboardInterrupt:
+    pass
+#cleanup
+wiringpi.wiringPiSetup() 
+print("Done")
