7c606f2824cf961f1e847ac05bdad4288c43271c
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2023-03-22 20:42:38 +0100

    integration 7 : working buttons with overruling

--- mouse_trap/button/button.py
+++ mouse_trap/button/button.py
@@ -2,18 +2,17 @@ import wiringpi as wp
 import time
 import threading
 
-button1_pin = 7
-button2_pin = 8
+green_pin = 8
+red_pin = 7
 
 
-
-def buttons(button1_pressed, button2_pressed, exit_event):
+def buttons(green_pressed, red_pressed, exit_event):
     while True:
-        if(wp.digitalRead(button1_pin) == 1): #input is active up (pull down)
-            button1_pressed.set()
+        if(wp.digitalRead(green_pin) == 1): #input is active up (pull down)
+            green_pressed.set()
             time.sleep(0.3) #anti bouncing
-        if(wp.digitalRead(button2_pin) == 1): #input is active up (pull down)
-            button2_pressed.set()
+        if(wp.digitalRead(red_pin) == 1): #input is active up (pull down)
+            red_pressed.set()
             time.sleep(0.3) #anti bouncing
         if exit_event.is_set():
             break
--- mouse_trap/mousetrap_v0.3.py
+++ mouse_trap/mousetrap_v0.3.py
@@ -5,26 +5,27 @@ import threading
 from door.dcmotor import pullDown, pullUp, fullStop
 from lock.stepper import lock, unlock
 from distance.distance_sensor import measure
+from button.button import buttons
 from screen.lcd import lcd
 
 # SETUP
 print("Start")
 echo_pin = 0
 trig_pin = 1
-button1_pin = 7
-button2_pin = 8
+green_pin = 8
+red_pin = 7
 dcmotor_pin1 = 2
 dcmotor_pin2 = 5
 speed = 100
 locked = False
 open = True
 must_open = False
-button1 = False
-button2 = False
+green = False
+red = False
 step_pins = [3, 4, 6, 9]
 exit_event = threading.Event()
-button1_pressed = threading.Event()
-button2_pressed = threading.Event()
+green_pressed = threading.Event()
+red_pressed = threading.Event()
 
 wp.wiringPiSetup()
 
@@ -42,23 +43,11 @@ wp.softPwmCreate(dcmotor_pin1, 0, 100)
 wp.softPwmCreate(dcmotor_pin2, 0, 100)
 
 # Set button pins as input
-wp.pinMode(button1_pin, 0)         # Set pin to mode 0 ( INPUT )
-wp.pinMode(button2_pin, 0)  
-
-
-def buttons(button1_pressed, button2_pressed, exit_event):
-    while True:
-        if(wp.digitalRead(button1_pin) == 1): #input is active up (pull down)
-            button1_pressed.set()
-            time.sleep(0.3) #anti bouncing
-        if(wp.digitalRead(button2_pin) == 1): #input is active up (pull down)
-            button2_pressed.set()
-            time.sleep(0.3) #anti bouncing
-        if exit_event.is_set():
-            break
+wp.pinMode(green_pin, 0)         # Set pin to mode 0 ( INPUT )
+wp.pinMode(red_pin, 0)  
 
 #create two new threads
-t1 = threading.Thread(target=buttons, args=(button1_pressed, button2_pressed, exit_event))
+t1 = threading.Thread(target=buttons, args=(green_pressed, red_pressed, exit_event))
 t2 = threading.Thread(target=lcd)
 
 #start the threads
@@ -68,25 +57,33 @@ t2.start()
 try:
 	while True:
 
-		if button1_pressed.is_set():
-			print("button 1 pressed")
-			button1 = True
-			button1_pressed.clear()
-		if button2_pressed.is_set():
-			print("button 2 pressed")
-			button2 = True
-			button2_pressed.clear()
+		if green_pressed.is_set():
+			print("Green button pressed")
+			green = True
+			green_pressed.clear()
+		if red_pressed.is_set():
+			print("Red button pressed")
+			red = True
+			red_pressed.clear()
 
 		measurement = measure()
 		
-		if button2 or measurement < 30 :
-			must_open = True
-			button2 = False
-		elif button1 or measurement >= 30 :
+		if measurement >= 30 :
+			print("Mouse in cage. Door must be closed")
 			must_open = False
-			button1 = False
-		else:
-			must_open = open
+			if green:
+				print("However, green button was pressed. Door must be opened")
+				must_open = True
+				green = False
+			# ook iets doen met red
+
+		else :
+			print("No mouse in cage. Door must be opened")
+			must_open = True
+			if red:
+				print("However, red button was pressed. Door must be closed")
+				must_open = False
+				red = False
 
 		if not must_open and open:   # catch
 			if locked == True:
