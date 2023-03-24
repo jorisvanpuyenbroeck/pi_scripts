7b4ec52d58310490afbcf03aae0f706a4f3035bc
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2023-03-24 21:11:00 +0100

    test webcam motion

--- mouse_trap/tests/camera_test.py
+++ mouse_trap/tests/camera_test.py
@@ -1,19 +1,69 @@
 ##pip install opencv-python
+##pip install flask
+##pip install numpy
 
+import ffmpeg
 import cv2
-import numpy as np
-#c = cv2.VideoCapture(1)
- 
-# while(1):
-#   _,f = c.read()
-#   cv2.imshow('e2',f)
-#   if cv2.waitKey(5)==27:
-#     break
- 
-# cv2.destroyAllWindows()
+import subprocess
 
-webcam = cv2.VideoCapture(1)
-return_value, image = webcam.read()
-rotate = cv2.rotate(image, cv2.ROTATE_180)
-cv2.imwrite('/home/orangepi/pi_scripts/mouse_trap/tests/mousetrap.bmp', rotate)
-del (webcam)
+video_format = "flv"
+server_url = "http://192.168.137.2:6666"
+
+def start_streaming(width, height,fps):
+    process = (
+        ffmpeg
+        .input('pipe:', format='rawvideo',codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(width, height))
+        .output(
+            server_url + '/stream',
+            #codec = "copy", # use same codecs of the original video
+            listen=1, # enables HTTP server
+            pix_fmt="yuv420p",
+            preset="ultrafast",
+            f=video_format
+        )
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+    )
+    return process
+
+def init_cap():
+    cap = cv2.VideoCapture(1)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    return cap, width, height
+
+def run():
+    cap, width, height = init_cap()
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    streaming_process = start_streaming(width, height,fps)
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            streaming_process.stdin.write(frame.tobytes())
+        else:
+            break
+    streaming_process.stdin.close()
+    streaming_process.wait()
+    cap.release()
+
+if __name__ == "__main__":
+    run()
+
+def stream_vnc():
+  webcam = cv2.VideoCapture(1)
+  while True:
+    ret, frame = webcam.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+  webcam.release()
+  cv2.destroyAllWindows()
+
+def take_picture():
+  webcam = cv2.VideoCapture(1)
+  ret, frame = webcam.read()
+  rotate = cv2.rotate(frame, cv2.ROTATE_180)
+  cv2.imwrite('/home/orangepi/pi_scripts/mouse_trap/tests/mousetrap.bmp', frame)
+  del (webcam)
+
+# take_picture()
