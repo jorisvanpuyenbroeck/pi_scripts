3afac55f178102317a5c7c7af7b3fb29b54ce712
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2023-03-24 19:38:34 +0100

    test snapshot cv2

--- mouse_trap/tests/camera_test.py
+++ mouse_trap/tests/camera_test.py
@@ -2,12 +2,18 @@
 
 import cv2
 import numpy as np
-c = cv2.VideoCapture(1)
+#c = cv2.VideoCapture(1)
  
-while(1):
-  _,f = c.read()
-  cv2.imshow('e2',f)
-  if cv2.waitKey(5)==27:
-    break
+# while(1):
+#   _,f = c.read()
+#   cv2.imshow('e2',f)
+#   if cv2.waitKey(5)==27:
+#     break
  
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
+
+webcam = cv2.VideoCapture(1)
+return_value, image = webcam.read()
+rotate = cv2.rotate(image, cv2.ROTATE_180)
+cv2.imwrite('/home/orangepi/pi_scripts/mouse_trap/tests/mousetrap.bmp', rotate)
+del (webcam)
